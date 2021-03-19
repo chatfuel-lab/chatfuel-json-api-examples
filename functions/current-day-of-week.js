ab31da3d982b410fbffdc0336c8bca6f2a1919d9
Evgeny Khokhlov <khokhlov.e.n@gmail.com>
2021-03-19 22:12:00 +0300

    init: add current-day-of-week lambda

---
+++ functions/current-day-of-week.js
@@ -0,0 +1,19 @@
+const days = ["Mon", "Tues", "Weds", "Thurs", "Fri", "Sat", "Sun"];
+const MINS_TO_MS = 60000;
+const HOURS_TO_MINS = 60;
+
+exports.handler = async (event, context) => {
+  // Only allow POST
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+  
+  const { timezone } = JSON.parse(event.body);
+  const now = new Date();
+  const adjustedDate = new Date(now.getTime() + now.getTimezoneOffset() * MINS_TO_MS + timezone * HOURS_TO_MINS * MINS_TO_MS);
+  
+  return {
+    statusCode: 200,
+    body: `{"set_attributes": {"day": "${days[adjustedDate.getDay() - 1]}", "hour": "${adjustedDate.getHours()}", "minute": "${adjustedDate.getMinutes()}"}}`,
+  };
+};
