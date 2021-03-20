98c1307022c03105fabb5b98b20be9e21e6bdcdd
evgeny <khokhlov.e.n@gmail.com>
2021-03-20 08:44:51 +0300

    refactor: use `JSON.stringify` instead of template string

--- functions/current-day-of-week.js
+++ functions/current-day-of-week.js
@@ -7,13 +7,21 @@ exports.handler = async (event, context) => {
   if (event.httpMethod !== "POST") {
     return { statusCode: 405, body: "Method Not Allowed" };
   }
-  
+
   const { timezone } = JSON.parse(event.body);
   const now = new Date();
   const adjustedDate = new Date(now.getTime() + now.getTimezoneOffset() * MINS_TO_MS + timezone * HOURS_TO_MINS * MINS_TO_MS);
-  
+
+  const response = {
+    set_attributes: {
+      day: days[adjustedDate.getDay() - 1],
+      hour: adjustedDate.getHours(),
+      minute: adjustedDate.getMinutes()
+    }
+  }
+
   return {
     statusCode: 200,
-    body: `{"set_attributes": {"day": "${days[adjustedDate.getDay() - 1]}", "hour": "${adjustedDate.getHours()}", "minute": "${adjustedDate.getMinutes()}"}}`,
+    body: JSON.stringify(response),
   };
 };
