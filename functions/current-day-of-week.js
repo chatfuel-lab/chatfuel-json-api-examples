47cbcd592117876843c7421a89cc040f0f230baf
evgeny <khokhlov.e.n@gmail.com>
2021-03-20 10:22:42 +0300

    fix: use global timezone not user specific

--- functions/current-day-of-week.js
+++ functions/current-day-of-week.js
@@ -2,13 +2,14 @@ const days = ["Mon", "Tues", "Weds", "Thurs", "Fri", "Sat", "Sun"];
 const MINS_TO_MS = 60000;
 const HOURS_TO_MINS = 60;
 
+const timezone = -5;
+
 exports.handler = async (event, context) => {
   // Only allow POST
   if (event.httpMethod !== "POST") {
     return { statusCode: 405, body: "Method Not Allowed" };
   }
 
-  const { timezone } = JSON.parse(event.body);
   const now = new Date();
   const adjustedDate = new Date(now.getTime() + now.getTimezoneOffset() * MINS_TO_MS + timezone * HOURS_TO_MINS * MINS_TO_MS);
 
