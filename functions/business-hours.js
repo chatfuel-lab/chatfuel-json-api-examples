5d622a1669d94f7ffb4ddd66f5ec6ac9c3f996db
evgeny <khokhlov.e.n@gmail.com>
2021-03-24 13:38:44 +0300

    fix: close -> closed typo

--- functions/business-hours.js
+++ functions/business-hours.js
@@ -29,7 +29,7 @@ exports.handler = async (event) => {
 
   const response = {
     set_attributes: {
-      business: businessIsOpen ? 'open' : 'close'
+      business: businessIsOpen ? 'open' : 'closed'
     }
   }
 
