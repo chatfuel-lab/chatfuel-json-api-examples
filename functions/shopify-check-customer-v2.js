2e3dadeee5fed4017561d9a16655a6ca15f41784
evgeny <khokhlov.e.n@gmail.com>
2021-05-05 19:27:51 +0300

    feat: add new version of `shopify-check-customer`

---
+++ functions/shopify-check-customer-v2.js
@@ -0,0 +1,39 @@
+const fetch = require('node-fetch');
+
+const handleCheckShopifyCustomer = async (event) => {
+  const payload = JSON.parse(event.body);
+
+  const { email, store_url, password } = payload;
+  const shopifyOrdersUrl = `https://${store_url}/admin/api/2021-04/orders.json?status=any&email=${email}`;
+
+  try {
+    const data = await fetch(shopifyOrdersUrl, {
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Shopify-Access-Token': password
+      }
+    }).then(r => r.json());
+
+    const { orders } = data;
+
+    return {
+      hasError: false,
+      response: {
+        set_attributes: {
+          customer: orders ? 'existing' : 'new'
+        }
+      }
+    }
+  } catch (error) {
+    return { hasError: true, errorMessage: `Error: ${error}` }
+  }
+}
+
+exports.handler = async (event) => {
+  const { hasError, errorMessage, response } = await handleCheckShopifyCustomer(event);
+
+  return {
+    statusCode: hasError ? 400 : 200,
+    body: hasError ? errorMessage : JSON.stringify(response)
+  }
+};
