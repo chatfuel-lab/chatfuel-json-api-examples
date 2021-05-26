d2ce800bd6ee9043ec7c4565385696b3926c2801
Christopher Hiester <chris@hiester.com>
2021-05-26 15:22:23 -0400

    clean up error response, incoming params in body

--- functions/verify-phone.ts
+++ functions/verify-phone.ts
@@ -1,30 +1,32 @@
-import { Handler } from '@netlify/functions';
+import { Handler, HandlerEvent } from '@netlify/functions';
 import fetch from 'node-fetch';
 
 // Verify a phone number
-export const handler: Handler = async event => {
-  const { access_key, number, country_code, format } = event.queryStringParameters || {};
-  console.log(access_key);
-
+export const handler: Handler = async (event: HandlerEvent) => {
+  const payload = JSON.parse(event.body);
+  const { access_key, number, country_code, format } = payload || {};
   const req = `http://apilayer.net/api/validate?access_key=${access_key}&number=${number}&country_code=${country_code}&format=${format}`;
   try {
     const res: any = await fetch(req);
     const data = await res.json();
-
     const response = {
       set_attributes: {
-        phoneValid: data.valid
+        phoneValid: data && data.valid ? data.valid : false
       }
     };
-
     return {
       statusCode: 200,
       body: JSON.stringify(response)
     };
   } catch (err) {
+    const errorResponse = {
+      set_attributes: {
+        phoneValid: false
+      }
+    };
     return {
-      statusCode: 500,
-      body: `ERROR: ${err.message}`
+      statusCode: 200,
+      body: JSON.stringify(errorResponse)
     };
   }
 };
