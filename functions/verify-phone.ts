ccacb45e571c37c4b33000d853685cf28e818d38
Christopher Hiester <chris@hiester.com>
2021-05-26 10:49:52 -0400

    verify phone using netlify function calling numverify validate endpoint

---
+++ functions/verify-phone.ts
@@ -0,0 +1,30 @@
+import { Handler } from '@netlify/functions';
+import fetch from 'node-fetch';
+
+// Verify a phone number
+export const handler: Handler = async event => {
+  const { access_key, number, country_code, format } = event.queryStringParameters || {};
+  console.log(access_key);
+
+  const req = `http://apilayer.net/api/validate?access_key=${access_key}&number=${number}&country_code=${country_code}&format=${format}`;
+  try {
+    const res: any = await fetch(req);
+    const data = await res.json();
+
+    const response = {
+      set_attributes: {
+        phoneValid: data.valid
+      }
+    };
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response)
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(`ERROR: ${err.message}`)
+    };
+  }
+};
