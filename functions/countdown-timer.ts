04b0d70320151ff004fa92beeeed178bfe5ca4a3
Christopher Hiester <chris@hiester.com>
2021-05-28 14:16:50 -0400

    countdown timer that accepts IANA timezone string

--- functions/countdown-timer.ts
+++ functions/countdown-timer.ts
@@ -1,52 +1,37 @@
 import { Handler, HandlerEvent } from '@netlify/functions';
-import fetch from 'node-fetch';
+import { months, parseTime, getDateInTimezone, getNowInTimezone } from './utils/date-utils';
 import { DateTime } from 'luxon';
 
-const months = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December'
-];
-
-// Verify a phone number
 export const handler: Handler = async (event: HandlerEvent) => {
   const payload = JSON.parse(event.body);
   const { month, day, year, time, timezone_short } = payload || {};
 
-  const now = DateTime.now();
+  // construct future date from user input
+  const userMonth = Number(months.indexOf(month));
+  const userTime = parseTime(time);
+  const futureDate = new Date(year, userMonth, day, userTime.hours, userTime.minutes);
 
-  // convert user's date to integer
-  const userMonth = Number(months.indexOf(month) + 1);
-  const userTime = time.match(/([01]?[0-9]|2[0-3]):([0-5][0-9])([A-Z][A-Z])/);
-  const userHour = userTime[3] === 'PM' ? Number(userTime[1]) + 12 : userTime[1];
-  const userMinute = Number(userTime[2].replace(/^0+/, ''));
-  const userDate = DateTime.local(year, userMonth, day, userHour, userMinute);
+  //time zone adjust
+  const tzFutureDate = timezone_short ? getDateInTimezone(timezone_short, futureDate) : futureDate;
+  const now = timezone_short ? getDateInTimezone(timezone_short, new Date()) : new Date();
 
-  // calculate diff between userDate and current date
-  const userDiff = userDate.diff(now, ['years', 'months', 'days', 'hours', 'minutes', 'seconds']);
-  console.log(userDiff.as('days'));
+  // set up for comparison
+  const luxonNow = DateTime.fromISO(now.toISOString());
+  const luxonFutureDate = DateTime.fromISO(tzFutureDate.toISOString());
+
+  // get the diff
+  const userDiff = luxonFutureDate.diff(luxonNow, ['years', 'months', 'days', 'hours', 'minutes', 'seconds']);
 
   const response = {
     set_attributes: {
-      time_remaining: userDate.diff(now).toObject().milliseconds > 0 ? true : false,
-      time_until: `${Math.floor(userDiff.as('days'))} days, ${userDiff.hours} hours, ${
-        userDiff.minutes
-      } minutes, ${Math.round(userDiff.seconds)} seconds`,
-      years_until: Math.floor(userDiff.as('years')),
-      months_until: Math.floor(userDiff.as('months')),
-      days_until: Math.floor(userDiff.as('days')),
-      hours_until: Math.floor(userDiff.as('hours')),
-      minutes_until: Math.floor(userDiff.as('minutes')),
-      seconds_until: Math.floor(userDiff.as('seconds'))
+      time_remaining: luxonFutureDate.diff(luxonNow).toObject().milliseconds > 0 ? true : false,
+      time_until: `${userDiff.days} days, ${userDiff.hours} hours, ${userDiff.minutes} minutes, ${userDiff.seconds} seconds`,
+      years_until: userDiff.years,
+      months_until: userDiff.months,
+      days_until: userDiff.days,
+      hours_until: userDiff.hours,
+      minutes_until: userDiff.minutes,
+      seconds_until: userDiff.seconds
     }
   };
   return {
