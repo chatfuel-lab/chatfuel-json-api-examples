c15c1ba81ebc469f871b4f47d53af107422e415c
Christopher Hiester <chris@hiester.com>
2021-05-27 23:29:37 -0400

    initial attempt at countdown timer

--- functions/verify-phone.ts
+++ functions/verify-phone.ts
@@ -5,7 +5,6 @@ import fetch from 'node-fetch';
 export const handler: Handler = async (event: HandlerEvent) => {
   const payload = JSON.parse(event.body);
   const { access_key, number, country_code, format } = payload || {};
-  console.log(number);
   const regex = /\D+/gm;
   const sanitizedNumber = number.toString().replace(regex, '');
   const req = `http://apilayer.net/api/validate?access_key=${access_key}&number=${sanitizedNumber}&country_code=${country_code}&format=${format}`;
