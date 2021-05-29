550128b5cabbcf93e5763c85ebf6b5867d9b1017
Christopher Hiester <chris@hiester.com>
2021-05-29 00:14:49 -0400

    revisions to time_until formatting

--- functions/countdown-timer.ts
+++ functions/countdown-timer.ts
@@ -1,6 +1,6 @@
 import { Handler, HandlerEvent } from '@netlify/functions';
 import { months, parseTime, getDateInTimezone, getNowInTimezone } from './utils/date-utils';
-import { DateTime } from 'luxon';
+import { DateTime, DurationUnits } from 'luxon';
 
 export const handler: Handler = async (event: HandlerEvent) => {
   const payload = JSON.parse(event.body);
@@ -18,13 +18,31 @@ export const handler: Handler = async (event: HandlerEvent) => {
   const luxonNow = DateTime.fromISO(now.toISOString());
   const luxonFutureDate = DateTime.fromISO(futureDate.toISOString());
 
+  const units: DurationUnits = ['years', 'months', 'days', 'hours', 'minutes', 'seconds'];
+
   // get the diff
-  const userDiff = luxonFutureDate.diff(luxonNow, ['years', 'months', 'days', 'hours', 'minutes', 'seconds']);
+  const userDiff = luxonFutureDate.diff(luxonNow, units);
+
+  // container for final formtted value for time_until
+  const timeUntil = [];
+
+  // make sure the first item in time_until does not have zero value
+  units.forEach(unitString => {
+    let first = true;
+    if (first && userDiff[unitString] === 0) {
+      return false;
+    } else {
+      timeUntil.push(`${userDiff[unitString]} ${userDiff[unitString] === 1 ? unitString.slice(0, -1) : unitString}`); // remove 's' if value is 1, e.g '1 day', not '1 days'
+      first = false;
+    }
+  });
+
+  timeUntil[timeUntil.length - 1] = `and ${timeUntil[timeUntil.length - 1]}`;
 
   const response = {
     set_attributes: {
       time_remaining: luxonFutureDate.diff(luxonNow).toObject().milliseconds > 0 ? true : false,
-      time_until: `${userDiff.days} days, ${userDiff.hours} hours, ${userDiff.minutes} minutes, ${userDiff.seconds} seconds`,
+      time_until: timeUntil.join(', '),
       years_until: userDiff.years,
       months_until: userDiff.months,
       days_until: userDiff.days,
